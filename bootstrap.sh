7b6298f514159b64f8709466d77cc003ad727253
Christian Noack <christian.noack@agile-methoden.de>
2016-01-24 00:12:13 +0100

    switch to debian due to internal annoying ubuntu errors

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,73 +4,66 @@
 echo 'setxkbmap -layout de ' >> ~vagrant/.profile # set german keyboard layout for PC
 echo 'setxkbmap -layout de ' >> ~vagrant/.bashrc # set german keyboard layout for  PC
 #echo 'setxkbmap -layout de -variant mac' >> ~vagrant/.profile # set german keyboard layout for mac
-#echo 'setxkbmap -layout de  -variant mac' >> ~vagrant/.bashrc # set german keyboard layout for mac
+#echo 'setxkbmap -layout de  -variant mac' >> ~vagrant/.bashrc # setx german keyboard layout for mac
 
 # allow sudo for user vagrant
-echo 'vagrant    ALL=(ALL:ALL) NOPASSWD: ALL' >> /etc/sudoers
-usermod -G admin vagrant
+#echo 'vagrant    ALL=(ALL:ALL) NOPASSWD: ALL' >> /etc/sudoers
+#usermod -G admin vagrant
 groupadd vboxfs
 usermod -a -G vboxfs vagrant
 
+apt-get update
+apt-get install -y mate-desktop-environment lightdm build-essential mate-terminal apt-transport-https software-properties-common
+
 # update guest additions do 5.0.12
 cd /tmp
-wget http://download.virtualbox.org/virtualbox/5.0.12/VBoxGuestAdditions_5.0.12.iso
-apt-get remove virtualbox-guest-x11 virtualbox-guest-utils unity-scope-virtualbox
- ./VBoxLinuxAdditions.run --  --force
+wget http://download.virtualbox.org/virtualbox/5.0.14/VBoxGuestAdditions_5.0.14.iso
+mount ./VBoxGuestAdditions_5.0.14.iso /mnt
+/mnt/VBoxLinuxAdditions.run
 
 
-# make partition for docker if it does not already exist
-docker_disk_is_formatted=`blkid /dev/sdb1`
-if [ -z "$docker_disk_is_formatted" ]; then
- # make partition
-  cat <<EOF | fdisk /dev/sdb
-n
-p
-1
 
-
-w
-EOF
-
-  # format partition
-  mkfs.ext4 /dev/sdb1
-  # automount partition on boot
-  mkdir -p /var/lib/docker
-  echo '/dev/sdb1    /var/lib/docker    ext4    defaults            1     2' >> /etc/fstab
-fi
-
-mount -a
+## make partition for docker if it does not already exist
+# docker_disk_is_formatted=`blkid /dev/sdb1`
+# if [ -z "$docker_disk_is_formatted" ]; then
+#  # make partition
+#   cat <<EOF | fdisk /dev/sdb
+# n
+# p
+# 1
+#
+#
+# w
+# EOF
+#   # format partition
+#   mkfs.ext4 /dev/sdb1
+#   # automount partition on boot
+#   mkdir -p /var/lib/docker
+#   echo '/dev/sdb1    /var/lib/docker    ext4    defaults            1     2' >> /etc/fstab
+# fi
+# mount -a
 
 # install software
 
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo 'deb https://apt.dockerproject.org/repo ubuntu-trusty main' > /etc/apt/sources.list.d/docker.list
 
-add-apt-repository ppa:webupd8team/java
+echo y | add-apt-repository ppa:webupd8team/java
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 add-apt-repository ppa:webupd8team/atom
 
 apt-get update
 
-apt-get install -y wget apt-cacher-ng build-essential libsdl1.2debian python-software-properties debconf-utils git-core zlib1g-dev postgresql libpq-dev zip sqlite3 libsqlite3-dev pgadmin3
+apt-get install -y wget apt-cacher-ng libsdl1.2debian python-software-properties debconf-utils git-core zlib1g-dev postgresql libpq-dev zip sqlite3 libsqlite3-dev pgadmin3
 echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | debconf-set-selections
 apt-get install --yes --force-yes -f oracle-java8-installer
 apt-get install -y google-chrome-stable nodejs npm
-apt-get install --yes --force-yes -f docker-engine
-apt-get install --yes --force-yes -f atom
-
-sudo apt-get install -y --no-install-recommends mate-desktop-environment
+apt-get install --yes --force-yes -f docker-engine atom
 
 curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-
-# remove ubuntu cloud services
-echo 'datasource_list: [ None ]' | sudo -s tee /etc/cloud/cloud.cfg.d/90_dpkg.cfg
-dpkg-reconfigure -f noninteractive cloud-init
-apt-get remove --purge cloud-init
-rm -rf /var/lib/cloud-init
-rm -rf /etc/cloud
+usermod -a -G docker vagrant
 
 # install rvm + ruby
 curl -L https://get.rvm.io > /tmp/rvm-install.sh
@@ -78,7 +71,7 @@ su - vagrant -c  'gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C27
 sudo -u vagrant -H sh -c 'bash /tmp/rvm-install.sh'
 su - vagrant -c 'rvm install 2.2.2'
 echo 'export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting' >> ~vagrant/.bashrc
-echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*' >> ~vagrant/.bashrc
+echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scriptss/rvm" # Load RVM into a shell session *as a function*' >> ~vagrant/.bashrc
 rm /tmp/rvm-install.sh
 
 
@@ -114,4 +107,3 @@ http-proxy = ${http_proxy}
 EOF
 fi
 chown -R vagrant ~vagrant
-usermod -a -G docker vagrant
