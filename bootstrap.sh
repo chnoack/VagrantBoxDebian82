885451466aa61dbd90be69047c5ee8a0ecb5f3fe
Christian Noack <christian.noack@agile-methoden.de>
2016-01-24 14:30:42 +0100

    fix key import for docker

--- bootstrap.sh
+++ bootstrap.sh
@@ -64,7 +64,7 @@ apt-get install -y google-chrome-stable nodejs npm
 
 # install docker
 
-apt-key adv --keyserer hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo 'deb https://apt.dockerproject.org/repo debian-jessie main' > /etc/apt/sources.list.d/docker.list
 apt-get update
 apt-get install --yes --force-yes -f docker-engine
